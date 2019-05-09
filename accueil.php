4097bfd239e8207977bba2cba9b84d2be647176c
Vincent <vincent.conta@gmail.com>
2019-05-09 11:09:02 +0200

    page accceuil et connexion

---
+++ accueil.php
@@ -0,0 +1,7 @@
+<?php
+include "bdd_connection.php";
+
+$template ="accueil";
+
+include "layout.phtml";
+
---
+++ connexion.php
@@ -0,0 +1,6 @@
+<?php
+include "bdd_connection.php";
+
+$template ="connexion";
+
+include "layout.phtml";
