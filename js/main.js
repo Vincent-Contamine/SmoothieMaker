2b5ab14f17d69d8cc444cb1e0e528cabde1ba88d
lovipre <narteta01@hotmail.fr>
2019-05-09 15:51:42 +0200

    connexion

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ function verify(e){
         e.preventDefault();
         $('<div class="alert alert-danger" role="alert">Veuillez remplir tous les champs</div>').insertAfter("h1");
     }
+    var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(reg.test(String($('#email').val()).toLowerCase()) == false){
+        $('<div class="alert alert-danger" role="alert">Veuillez saisir un email valide</div>').insertAfter("h1");
+    }
+}
+
+function verifyI(e){
+    $('.alert').hide();
+    if ($('#email').val().length == 0 || $('#password').val().length == 0 || $('#cpassword').val().length == 0 || $('#LastName').val().length == 0 || $('#FirstName').val().length == 0)
+    {
+        e.preventDefault();
+        $('<div class="alert alert-danger" role="alert">Veuillez remplir tous les champs</div>').insertAfter("h1");
+    }
+    var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(reg.test(String($('#email').val()).toLowerCase()) == false){
+        $('<div class="alert alert-danger" role="alert">Veuillez saisir un email valide</div>').insertAfter("h1");
+    }
 }
 
 
@@ -14,4 +31,9 @@ $(document).ready(function(){
     {
         $('#connect').submit(verify);
     }
+    if (window.location.href.match('inscription.php') != null)
+    {
+        $('#connect').submit(verifyI);
+    }
 });
+
