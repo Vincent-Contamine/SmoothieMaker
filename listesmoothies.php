1e4e5925a74038fdc1221c70f17af5df6b5654a8
Vincent <vincent.conta@gmail.com>
2019-05-09 16:51:05 +0200

    listeSmoothie

--- listesmoothies.php
+++ listesmoothies.php
@@ -2,4 +2,8 @@
 include 'bdd_connection.php';
 $template="listesmoothies";
 
+    $requete = $pdo->prepare("SELECT `Id`, `Name`, `Photo`, `Mois` FROM `Recette` WHERE mois = '01' OR mois = '02' OR mois = '03'");
+    $requete->execute();
+    $smoothies = $requete->fetchAll();
+
 include 'layout.phtml';
