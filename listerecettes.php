06b8d521f2fb0771370acc3fb8323cbb288f2bc5
lovipre <narteta01@hotmail.fr>
2019-05-09 11:06:26 +0200

    liste recette et smoothie

---
+++ listerecettes.php
@@ -0,0 +1,5 @@
+<?php 
+include 'bdd_connection.php';
+$template="listerecettes";
+
+include 'layout.phtml';
---
+++ listesmoothies.php
@@ -0,0 +1,5 @@
+<?php 
+include 'bdd_connection.php';
+$template="listesmoothies";
+
+include 'layout.phtml';
